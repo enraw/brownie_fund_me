07ff4e2a44ac7073562fd9871f835e54e96ce385
enraw <96726009+enraw@users.noreply.github.com>
2021-12-30 13:30:18 +0100

    Update fund_and_withdraw.py
    entrance_fee = fund_me.getEntranceFee() 
    -->
    entrance_fee = fund_me.get_entrance_fee()
    
    otherwise 'attribute error'
    
    cheers for the course!

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -5,7 +5,7 @@ from scripts.helpful_scripts import get_account
 def fund():
     fund_me = FundMe[-1]
     account = get_account()
-    entrance_fee = fund_me.getEntranceFee()
+    entrance_fee = fund_me.get_entrance_fee()
     print(entrance_fee)
     print(f"The current entry fee is {entrance_fee}")
     print("Funding")
